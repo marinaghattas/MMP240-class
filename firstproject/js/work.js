9a1a78c0ee9cb7698c3e7e9bef423f6c00d508f4
marinaghattas <marina.ghattas@f706-12-34146.bmcc.cuny.edu>
2016-03-07 13:18:21 -0500

    final dit

--- firstproject/js/work.js
+++ firstproject/js/work.js
@@ -4,13 +4,15 @@ $(document).ready(function (){
 
     $('.hide-title img').mouseenter(function (){
         $(this).siblings().animate({
-            opacity: "1"
+            opacity: "1",
+            right:"50px"
         });
     });
 
     $('.hide-title img').mouseleave(function (){
         $(this).siblings().animate({
-            opacity: "0"
+            opacity: "0",
+            right:"0"
         });
     });
 
